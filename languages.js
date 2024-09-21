73a67eb864293524e4be863aa03f875dc829a566
David Andrés <skyvidas@gmail.com>
2024-09-21 12:25:52 +0200

    prueba2

--- languages.js
+++ languages.js
@@ -1,7 +1,63 @@
 const translations = {
     es: {
         title: "Gymkhana QR",
-        placeHolder:"Escribe tu respuesta aquí...",
+        pruebas: [
+            {
+                prueba:"OFICINA DE TURISMO",
+                pregunta: "¿POR QUÉ LA LLAMAN LA VILLA DE LAS TRES MENTIRAS?",
+                respuesta: "Ni Santa ni llana ni tiene Mar"
+            },
+            {
+                prueba:"MUSEO DE LA TORTURA",
+                pregunta: "Bienvenido a este tenebroso lugar.",
+                pregunta1: "Para continuar deberás deberás hacerte una foto simulando algo de lo que se representa dentro del museo.",
+                respuesta: ""
+            },
+            {
+                prueba:"CASA DE LOS HOMBRONES",
+                pregunta: "¿CÓMO SE LLAMA ESE EDIFICIO Y POR QUÉ?",
+                respuesta: "La casa de los hombrones, por los hombres que hay a ambos lados del escudo."
+            },
+            {
+                prueba:"LAVADERO ",
+                pregunta: "",
+                respuesta: ""
+            },
+            {
+                prueba:"CASA QUEVEDO",
+                pregunta: "¿QUÉ PASA SI NO TOMAS UN VASO DE LECHE CON BIZCOCHO?",
+                respuesta: "No te casas"
+            },
+            {
+                prueba:"MUSEO JESÚS OTERO",
+                pregunta: "¿A QUIÉN REPRESENTAN LAS SIETE CARAS TALLADAS EN PIEDRA?",
+                respuesta: "Su familia"
+            },
+            {
+                prueba:"COLEGIATA",
+                pregunta: "¿LAS RELIQUIAS DE QUÉ SANTA SE ENCUENTRAN EN SU INTERIOR?",
+                respuesta: "Santa Juliana"
+            },
+            {
+                prueba:"PLAZA MAYOR",
+                pregunta: "¿QUÉ TRES BANDERAS ONDEAN EN EL AYUNTAMIENTO?",
+                respuesta: "Santillana, España, Cantabria y Europa."
+            },
+            {
+                prueba:"CASA DEL AGUILA Y LA PARRA",
+                pregunta: "HACERSE FOTO DIVERTIDA CON EL BISONTE.",
+                respuesta: ""
+            },
+            {
+                prueba:"OFICINA DE TURISMO",
+                pregunta: "¿CON QUÉ DOS CIUDADES EUROPEAS ESTÁ HERMANADA SANTILLANA?",
+                respuesta: "Borgolavezzaro (Italia) y Le Dorat (Francia)"
+            }
+        ],
+        tryAgain: "Resuesta Incorrecta, inténtalo de nuevo"
+    },
+    en: {
+        title: "Gymkhana QR",
         pruebas: [
             {
                 prueba:"OFICINA DE TURISMO",
@@ -56,94 +112,58 @@ const translations = {
         ],
         tryAgain: "Resuesta Incorrecta, inténtalo de nuevo"
     },
-    en: {
-        title: "QR Gymkhana",
-        pruebas: [
-            {
-                pregunta: "START TOURIST OFFICE. WHY IS IT CALLED THE VILLAGE OF THE THREE LIES?",
-                respuesta: "Neither Saint, nor flat, nor does it have a sea."
-            },
-            {
-                pregunta: "TORTURE MUSEUM. YOU MUST GO DOWN CALLE JESÚS OTERO, TURN LEFT ONTO CALLE MARTÍN CLISSON, AND TAKE A PHOTO SIMULATING SOMETHING THAT IS REPRESENTED INSIDE THE MUSEUM.",
-                respuesta: ""
-            },
-            {
-                pregunta: "HOMBRONES HOUSE. WHAT IS THE NAME OF THIS BUILDING AND WHY?",
-                respuesta: "The house of the hombrones, because of the men on both sides of the coat of arms."
-            },
-            {
-                pregunta: "WASHHOUSE (GOING DOWN CALLE CANTÓN).",
-                respuesta: ""
-            },
-            {
-                pregunta: "QUEVEDO HOUSE. WHAT HAPPENS IF YOU DON'T DRINK A GLASS OF MILK WITH CAKE?",
-                respuesta: "You won't get married."
-            },
-            {
-                pregunta: "JESÚS OTERO MUSEUM. WHO DO THE SEVEN FACES CARVED IN STONE REPRESENT?",
-                respuesta: "His family."
-            },
-            {
-                pregunta: "COLLEGIATE CHURCH. THE RELICS OF WHICH SAINT ARE IN THE INTERIOR?",
-                respuesta: "Saint Juliana."
-            },
-            {
-                pregunta: "MAIN SQUARE. WHICH THREE FLAGS FLY ON THE TOWN HALL?",
-                respuesta: "Santillana, Spain, Cantabria, and Europe."
-            },
-            {
-                pregunta: "HOUSE OF THE EAGLE AND THE VINE. TAKE A FUNNY PHOTO WITH THE BISON.",
-                respuesta: ""
-            },
-            {
-                pregunta: "TOURIST OFFICE. WITH WHICH TWO EUROPEAN CITIES IS SANTILLANA TWINNED?",
-                respuesta: "Borgolavezzaro (Italy) and Le Dorat (France)."
-            }
-        ],
-        tryAgain: "Resuesta Incorrecta, inténtalo de nuevo"
-    },
     fr: {
         title: "Gymkhana QR",
         pruebas: [
             {
-                pregunta: "DÉPART OFFICE DE TOURISME. POURQUOI L'APPELLE-T-ON LE VILLAGE DES TROIS MENSONGES?",
-                respuesta: "Ni sainte, ni plate, ni elle n'a de mer."
+                prueba:"OFICINA DE TURISMO",
+                pregunta: "¿POR QUÉ LA LLAMAN LA VILLA DE LAS TRES MENTIRAS?",
+                respuesta: "Ni Santa ni llana ni tiene Mar"
             },
             {
-                pregunta: "MUSÉE DE LA TORTURE. VOUS DEVEZ DESCENDRE LA RUE JESÚS OTERO, TOURNER À GAUCHE SUR LA RUE MARTÍN CLISSON ET PRENDRE UNE PHOTO SIMULANT QUELQUE CHOSE QUI EST REPRÉSENTÉ À L'INTÉRIEUR DU MUSÉE.",
+                prueba:"MUSEO DE LA TORTURA",
+                pregunta: "TENDRÁS QUE DIRIGIRTE POR LA CALLE JESÚS OTERO Y GIRAR HACIA LA IZQUIERDA POR LA CALLE MARTÍN CLISSON Y HACERTE UNA FOTO SIMULANDO ALGO DE LO QUE SE REPRESENTA DENTRO DEL MUSEO.",
                 respuesta: ""
             },
             {
-                pregunta: "MAISON DES HOMBRONES. COMMENT S'APPELLE CE BÂTIMENT ET POURQUOI?",
-                respuesta: "La maison des hombrones, à cause des hommes des deux côtés du blason."
+                prueba:"CASA DE LOS HOMBRONES",
+                pregunta: "¿CÓMO SE LLAMA ESE EDIFICIO Y POR QUÉ?",
+                respuesta: "La casa de los hombrones, por los hombres que hay a ambos lados del escudo."
             },
             {
-                pregunta: "LAVOIR (DESCENTE PAR LA RUE CANTÓN).",
+                prueba:"LAVADERO ",
+                pregunta: "",
                 respuesta: ""
             },
             {
-                pregunta: "MAISON QUEVEDO. QUE SE PASSE-T-IL SI VOUS NE BUVEZ PAS UN VERRE DE LAIT AVEC UN GÂTEAU?",
-                respuesta: "Vous ne vous marierez pas."
+                prueba:"CASA QUEVEDO",
+                pregunta: "¿QUÉ PASA SI NO TOMAS UN VASO DE LECHE CON BIZCOCHO?",
+                respuesta: "No te casas"
             },
             {
-                pregunta: "MUSÉE JESÚS OTERO. QUI REPRÉSENTENT LES SEPT VISAGES SCULPTÉS DANS LA PIERRE?",
-                respuesta: "Sa famille."
+                prueba:"MUSEO JESÚS OTERO",
+                pregunta: "¿A QUIÉN REPRESENTAN LAS SIETE CARAS TALLADAS EN PIEDRA?",
+                respuesta: "Su familia"
             },
             {
-                pregunta: "COLLÉGIALE. LES RELIQUES DE QUELLE SAINTE SE TROUVENT À L'INTÉRIEUR?",
-                respuesta: "Sainte Julienne."
+                prueba:"COLEGIATA",
+                pregunta: "¿LAS RELIQUIAS DE QUÉ SANTA SE ENCUENTRAN EN SU INTERIOR?",
+                respuesta: "Santa Juliana"
             },
             {
-                pregunta: "PLACE PRINCIPALE. QUELLES TROIS DRAPEAUX FLOTTENT À L'HÔTEL DE VILLE?",
-                respuesta: "Santillana, Espagne, Cantabrie et Europe."
+                prueba:"PLAZA MAYOR",
+                pregunta: "¿QUÉ TRES BANDERAS ONDEAN EN EL AYUNTAMIENTO?",
+                respuesta: "Santillana, España, Cantabria y Europa."
             },
             {
-                pregunta: "MAISON DE L'AIGLE ET DE LA VIGNE. PRENEZ UNE PHOTO DRÔLE AVEC LE BISON.",
+                prueba:"CASA DEL AGUILA Y LA PARRA",
+                pregunta: "HACERSE FOTO DIVERTIDA CON EL BISONTE.",
                 respuesta: ""
             },
             {
-                pregunta: "OFFICE DE TOURISME. AVEC QUELLES DEUX VILLES EUROPÉENNES SANTILLANA EST-ELLE JUMELÉE?",
-                respuesta: "Borgolavezzaro (Italie) et Le Dorat (France)."
+                prueba:"OFICINA DE TURISMO",
+                pregunta: "¿CON QUÉ DOS CIUDADES EUROPEAS ESTÁ HERMANADA SANTILLANA?",
+                respuesta: "Borgolavezzaro (Italia) y Le Dorat (Francia)"
             }
         ],
         tryAgain: "Resuesta Incorrecta, inténtalo de nuevo"
